59fc23eee0003aaf2a1c9d1a8bce7832d47889cf
Seokyung <xuejd17@gmail.com>
2023-10-26 18:06:44 +0900

    feat: App, MarketScreen 컴포넌트 분리

--- App.jsx
+++ App.jsx
@@ -1,54 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import EventSource from 'react-native-sse';
-import {API_URL} from '@env';
-
+import React from 'react';
 import {
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import MarketScreen from './src/screens/MarketScreen';
 
 function App() {
-  const [listening, setListening] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    if (!listening) {
-      setListening(true);
-      const eventSource = new EventSource(API_URL);
-
-      eventSource.addEventListener('open', event => {
-        console.log('Server Connected!');
-      });
-
-      eventSource.addEventListener('sse.auction_viewed', event => {
-        const parsedData = JSON.parse(event.data);
-        setData(prev => [...prev, parsedData]);
-      });
-
-      eventSource.addEventListener('error', event => {
-        if (event.type === 'error') {
-          console.error('Connection error:', event.message);
-        } else if (event.type === 'exception') {
-          console.error('Error:', event.message, event.error);
-        }
-      });
-
-      return () => {
-        eventSource.addEventListener('close', event => {
-          console.log('Close Connection.');
-        });
-      };
-    }
-  }, [listening]);
-
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -66,60 +28,11 @@ function App() {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Text>Flea:Auction Demo App</Text>
-          <Text>마켓 - 진행중</Text>
-          <View style={styles.cardContainer}>
-            {data.map((item, idx) => {
-              return (
-                <View key={idx} style={styles.card}>
-                  <Text style={styles.card.cardText}>
-                    Auction Id: {item.auctionId}
-                  </Text>
-                  <Text style={styles.card.cardText}>
-                    View Count: {item.viewCount}
-                  </Text>
-                </View>
-              );
-            })}
-          </View>
+          <MarketScreen isDarkMode={isDarkMode} />
         </View>
       </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  cardContainer: {
-    flex: 1,
-    padding: 8,
-    backgroundColor: 'aliceblue',
-  },
-  card: {
-    margin: 8,
-    padding: 12,
-    borderRadius: 5,
-    backgroundColor: '#555',
-    cardText: {
-      fontSize: 16,
-      color: '#fff',
-    },
-  },
-});
 
 export default App;
---
+++ src/screens/MarketScreen.jsx
@@ -0,0 +1,102 @@
+import React, {useState, useEffect} from 'react';
+import EventSource from 'react-native-sse';
+import {API_URL} from '@env';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function MarketScreen({isDarkMode}) {
+  const [listening, setListening] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (!listening) {
+      setListening(true);
+      const eventSource = new EventSource(API_URL);
+
+      eventSource.addEventListener('open', event => {
+        console.log('Server Connected!');
+      });
+
+      eventSource.addEventListener('sse.auction_viewed', event => {
+        const parsedData = JSON.parse(event.data);
+        setData(prev => [...prev, parsedData]);
+      });
+
+      eventSource.addEventListener('error', event => {
+        if (event.type === 'error') {
+          console.error('Connection error:', event.message);
+        } else if (event.type === 'exception') {
+          console.error('Error:', event.message, event.error);
+        }
+      });
+
+      return () => {
+        eventSource.addEventListener('close', event => {
+          console.log('Close Connection.');
+        });
+      };
+    }
+  }, [listening]);
+
+  return (
+    <View style={marketScreenStyles.marketContainer}>
+      <Text style={marketScreenStyles.marketContainer.marketTitle}>
+        Flea:Auction Demo App
+      </Text>
+      <Text style={marketScreenStyles.marketContainer.marketStatus}>
+        마켓 - 진행중
+      </Text>
+      <View style={marketScreenStyles.cardContainer}>
+        {data.map((item, idx) => {
+          return (
+            <View key={idx} style={marketScreenStyles.card}>
+              <Text style={marketScreenStyles.card.cardText}>
+                Auction Id: {item.auctionId}
+              </Text>
+              <Text style={marketScreenStyles.card.cardText}>
+                View Count: {item.viewCount}
+              </Text>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+}
+
+const marketScreenStyles = StyleSheet.create({
+  marketContainer: {
+    padding: 8,
+    backgroundColor: '#333',
+    marketTitle: {
+      margin: 6,
+      color: '#fff',
+      fontSize: 24,
+      fontWeight: 700,
+    },
+    marketStatus: {
+      margin: 6,
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 500,
+    },
+  },
+  cardContainer: {
+    // flex: 1,
+    margin: 4,
+    padding: 8,
+    backgroundColor: 'aliceblue',
+  },
+  card: {
+    margin: 8,
+    padding: 12,
+    borderRadius: 5,
+    backgroundColor: '#555',
+    cardText: {
+      fontSize: 16,
+      color: '#fff',
+    },
+  },
+});
+
+export default MarketScreen;
