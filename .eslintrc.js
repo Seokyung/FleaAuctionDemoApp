90a6dd7bd802de98ef8a292c65d2dabcf63b589d
Seokyung <xuejd17@gmail.com>
2023-10-26 18:06:07 +0900

    feat: eslint, prettier 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,46 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    es6: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2, {SwitchCase: 1}],
+    quotes: ['error', 'single', {avoidEscape: true}],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
